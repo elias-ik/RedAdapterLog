2f712df98e4e2450b1c372ee755c8bf3534defd8
Elias <info@elias.dev>
2024-06-07 13:17:56 +0200

    add list and delete

--- RedAdapterLog/Program.cs
+++ RedAdapterLog/Program.cs
@@ -13,13 +13,13 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
+var realApiKey = Environment.GetEnvironmentVariable("APIKEY") ?? "default";
 app.UseHttpsRedirection();
 var path = Environment.GetEnvironmentVariable("STORE_PATH");
 path = path != null ? path + "/" : null;
 app.MapGet("/log", ([FromQuery]string apiKey,[FromQuery] string file, [FromQuery] string log) =>
 {
-    if(apiKey != "yeahbuddy") return "Invalid API Key";
+    if(apiKey != realApiKey) return "Invalid API Key";
     if (path is not null)
     {
         if (!Directory.Exists(path))
@@ -35,7 +35,7 @@ app.MapGet("/log", ([FromQuery]string apiKey,[FromQuery] string file, [FromQuery
 
 app.MapGet("/read", ([FromQuery]string apiKey, [FromQuery] string file) =>
 {
-    if(apiKey != "yeahbuddy") return "Invalid API Key";
+    if(apiKey != realApiKey) return "Invalid API Key";
     try
     {
         return File.ReadAllText((path ?? "")+ file);
@@ -48,6 +48,31 @@ app.MapGet("/read", ([FromQuery]string apiKey, [FromQuery] string file) =>
 .WithName("Read File")
 .WithOpenApi();
 
+app.MapGet("/list", ([FromQuery] string apiKey) =>
+{
+    if(apiKey != realApiKey) return "Invalid API Key";
+    return string.Join(Environment.NewLine, Directory.GetFiles(path ?? ""));
+})
+.WithName("List All Files")
+.WithOpenApi();
 
+app.MapGet("/list-urls", ([FromQuery] string apiKey) =>
+{
+    if(apiKey != realApiKey) return "Invalid API Key";
+    var files = Directory.GetFiles(path ?? "");
+    var urls = files.Select(x => $"https://red-tester-log.s1.elias.dev/read?apiKey=${realApiKey}&file=${x}");
+    return string.Join(Environment.NewLine, urls);
+})
+.WithName("List All Files As Urls")
+.WithOpenApi();
+
+app.MapGet("/delete", ([FromQuery] string apiKey) =>
+{
+    if(apiKey != realApiKey) return "Invalid API Key";
+    File.Delete(path ?? "");
+    return "Deleted";
+})
+.WithName("Delete File")
+.WithOpenApi();
 
 app.Run();
